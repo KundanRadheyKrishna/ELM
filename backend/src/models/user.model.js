fb2e802e4ab8420f7ad0dec8538c9f2614e6485e
KundanRadheyKrishna <kundankumarsinha14@gmail.com>
2026-08-08 01:28:48 -0700

    change usermodel.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -10,3 +10,5 @@ const userSchema = new mongoose.Schema({
 },{timestamps: true})
 
 export const User = mongoose.model("User", userSchema)
+
+// dekho ab jo bol rahe hai usko memory main store kar lena ok  admion  eploye ko add karega employe apna name  emial dega goggle ka ok or admin ke pass jayega fir user ko add akrega admin pcuhega ki kon ho thum tho naem  email dega or kon sa departemnt main ho or 
