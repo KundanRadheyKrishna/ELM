52718d1a321d30dd970419fd711840d58748858d
KundanRadheyKrishna <kundankumarsinha14@gmail.com>
2026-08-17 04:36:21 -0700

    usermodle.js main maine jwt banaya intigrate kiya

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+import { jwt } from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -39,5 +41,29 @@ const userSchema = new mongoose.Schema({
         default: null
     }
 }, { timestamps: true })
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        email: this.email,
+        role: this.role
+    }, process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id
+    }, process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
+}
 
 export const User = mongoose.model("User", userSchema)
